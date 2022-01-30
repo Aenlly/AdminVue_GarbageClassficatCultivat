0087beb3414b5f7e13c6deafcc58db24b67799f6
Aenlly <2645274834@qq.com>
2022-01-30 22:23:13 +0800

    积分信息管理-积分头衔管理所需样式与功能对接完成

--- src/views/integral/IntegralTitle.vue
+++ src/views/integral/IntegralTitle.vue
@@ -9,7 +9,7 @@
           <!-- 搜索区开始 -->
           <el-col :span="6">
             <el-input v-model="text" placeholder="请输入搜索内容">
-              <template #prepend> 类型名称 </template>
+              <template #prepend> 头衔名称 </template>
               <template #append>
                 <el-button @click="queryBy">
                   <el-icon><search /></el-icon>
@@ -36,31 +36,24 @@
               :data="data.records"
               style="width: 100%"
               @selection-change="handleSelectionChange"
-              max-height="550"
+              max-height="620"
               border
               stripe
             >
               <el-table-column type="selection" width="55" />
               <el-table-column fixed type="index" label="序号" width="60" />
-              <el-table-column prop="typeName" label="类型名称" />
-              <el-table-column label="图标" width="100">
-                <template #default="{ row }">
-                  <el-tooltip content="单击预览" placement="bottom">
-                    <el-image
-                      :src="httpResource + row.imgUrl"
-                      style="height: 80px"
-                      :preview-src-list="[httpResource + row.imgUrl]"
-                      fit="contain"
-                    ></el-image>
-                  </el-tooltip>
-                </template>
-              </el-table-column>
+              <el-table-column prop="pointsName" label="类型名称" />
               <el-table-column
-                prop="giftShow"
-                label="小程序是否显示"
+                prop="pointsRequire"
+                label="所需积分"
                 width="150"
               />
-              <el-table-column prop="createTime" label="创建时间" width="160" />
+              <el-table-column
+                prop="createTime"
+                label="创建时间"
+                width="160"
+                sortable
+              />
               <el-table-column label="操作" fixed="right" width="120">
                 <template #default="{ row }">
                   <el-tooltip content="编辑数据" placement="bottom">
@@ -76,7 +69,7 @@
                       type="danger"
                       icon="el-icon-delete"
                       size="small"
-                      @click="deleteById(row.id)"
+                      @click="deleteById(row.pointsId)"
                     ></el-button>
                   </el-tooltip>
                 </template>
@@ -96,6 +89,88 @@
       <!-- 卡片结束 -->
     </el-col>
   </el-row>
+
+  <!-- 新增数据弹窗开始 -->
+  <el-dialog v-model="dialogCreateVisible" title="新增数据">
+    <el-row>
+      <el-col :span="12" :offset="6">
+        <el-form
+          :model="create"
+          ref="createForm"
+          :rules="formRules"
+          label-position="right"
+          label-width="100px"
+        >
+          <el-form-item label="头衔名称" prop="pointsName">
+            <el-input
+              v-model="create.pointsName"
+              placeholder="请输入头衔名称"
+              show-word-limit
+              maxlength="10"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="所需积分" prop="pointsRequire">
+            <el-input-number
+              v-model="create.pointsRequire"
+              :min="1"
+              placeholder="请输入头衔所需积分"
+              style="width: 230px"
+            />
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <!-- 创建弹窗底部区域 -->
+    <template #footer>
+      <span>
+        <el-button @click="dialogCreateVisible = false">取消</el-button>
+        <el-button type="primary" @click="onCreate">确认</el-button>
+      </span>
+    </template>
+    <!-- 创建弹窗底部区域 -->
+  </el-dialog>
+  <!-- 新增数据弹窗结束 -->
+
+  <!-- 编辑弹窗开始 -->
+  <el-dialog v-model="dialogEditVisible" title="编辑数据">
+    <el-row>
+      <el-col :span="12" :offset="6">
+        <el-form
+          :model="edit"
+          :rules="formRules"
+          ref="editForm"
+          label-position="right"
+          label-width="100px"
+        >
+          <el-form-item label="头衔名称" prop="pointsName">
+            <el-input
+              v-model="edit.pointsName"
+              placeholder="请输入头衔名称"
+              show-word-limit
+              maxlength="10"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="所需积分" prop="pointsRequire">
+            <el-input-number
+              v-model="edit.pointsRequire"
+              :min="1"
+              placeholder="请输入头衔所需积分"
+              style="width: 230px"
+            />
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <!-- 编辑弹窗底部区域 -->
+    <template #footer>
+      <span>
+        <el-button @click="dialogEditVisible = false">取消</el-button>
+        <el-button type="primary" @click="onEdit">确认</el-button>
+      </span>
+    </template>
+    <!-- 编辑弹窗底部区域 -->
+  </el-dialog>
+  <!-- 编辑弹窗结束 -->
 </template>
 
 <script>
@@ -103,16 +178,45 @@ import Breadcrumb from "../../components/Breadcrumb.vue";
 import Pagination from "../../components/Pagination.vue";
 import qs from "qs";
 
+// 校验规则
+const formRules = {
+  pointsName: [
+    {
+      required: true, //必填
+      message: "请输入头衔名称", //提示
+      trigger: "blur", //鼠标离开时触发
+    },
+    {
+      min: 1, //最小长度
+      max: 10, //最大长度
+      message: "长度在 1 至 10 个字符", //提示
+      trigger: "blur",
+    },
+  ],
+  pointsRequire: [
+    {
+      required: true, //必填
+      message: "请输入兑换所需积分", //提示
+      trigger: "blur", //鼠标离开时触发
+    },
+    {
+      type: "number",
+      message: "必须是数字", //提示
+      trigger: "blur",
+    },
+  ],
+};
+
 export default {
   components: { Breadcrumb, Pagination },
   data() {
     return {
-      breadcrumb: [{ name: "礼品信息管理" }, { name: "礼品类型管理" }],
-      getListUrl: "/gift-type/getList", //获取礼品信息的后台接口
-      delByIdsUrl: "gift-type/delByIds", //批量删除的后台接口
-      delByIdUrl: "/gift-type/delById", //单一删除数据的后台接口
-      createUrl: "gift-type/create", //新增数据的后台接口
-      updateUrl: "gift-type/update", //修改数据的后台接口
+      breadcrumb: [{ name: "积分信息管理" }, { name: "积分头衔管理" }],
+      getListUrl: "points/getList", //获取信息数据的后台接口
+      delByIdsUrl: "points/delByIds", //批量删除的后台接口
+      delByIdUrl: "points/delById", //单一删除数据的后台接口
+      createUrl: "points/create", //新增数据的后台接口
+      updateUrl: "points/update", //修改数据的后台接口
       dialogCreateVisible: false, //创建数据的对话框
       dialogEditVisible: false, //编辑数据的对话框
       text: "",
@@ -122,6 +226,7 @@ export default {
         size: 10, //每页数据量
         total: 0, //总数据量
       },
+      formRules: formRules, //创建弹窗的验证规则
       create: {
         giftName: "",
         giftDesc: "",
@@ -163,7 +268,7 @@ export default {
     // 选择时触发事件
     handleSelectionChange(val) {
       // 统计所选id
-      this.selectIds = val.map((item) => item.id);
+      this.selectIds = val.map((item) => item.pointsId);
     },
 
     // 删除所选触发批量删除事件
@@ -225,6 +330,57 @@ export default {
           }
         })
         .catch(() => {});
+    },
+    // 新增数据弹窗确认按钮事件
+    onCreate() {
+      this.$refs.createForm.validate(async (valid) => {
+        if (!valid) {
+          this.$message.error("请填写正确信息!");
+          return;
+        } else {
+          const { data: res } = await this.axios.post(
+            this.createUrl,
+            qs.stringify(this.create)
+          );
+          if (res.code == 200) {
+            this.$message.success("新增数据成功!");
+            if (res.data === true) {
+              this.dialogCreateVisible = false;
+              // 重置表单
+              this.$refs.createForm.resetFields();
+              this.queryBy();
+              return;
+            }
+          }
+          this.$message.error("新增数据失败!");
+        }
+      });
+    },
+    // 编辑确认按钮事件
+    onEdit() {
+      // 校验表单
+      this.$refs.editForm.validate(async (valid) => {
+        if (!valid) {
+          this.$message.error("请填写正确信息!");
+          return;
+        } else {
+          const { data: res } = await this.axios.put(
+            this.updateUrl,
+            qs.stringify(this.edit)
+          );
+          if (res.code == 200) {
+            if (res.data === true) {
+              this.$message.success("保存数据成功!");
+              // 关闭弹窗
+              this.dialogEditVisible = false;
+              // 刷新数据
+              this.queryBy();
+              return;
+            }
+          }
+          this.$message.error("保存数据失败!");
+        }
+      });
     },
   },
 };
